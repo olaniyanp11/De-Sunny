6f13a045b2e7bcbcd81684ca533935cb8670e994
olaniyanp11 <olaniyanp11@gmail.com>
2026-03-16 23:21:00 +0100

    optimised version

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
+
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
